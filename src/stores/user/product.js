838b8026f85cb84f98f4b427283824c842c39683
wachira <wachirastudeo@gmail.com>
2024-08-15 10:45:18 +0700

    read product store

--- src/stores/user/product.js
+++ src/stores/user/product.js
@@ -1,17 +1,26 @@
-import { ref, computed } from 'vue';
 import { defineStore } from 'pinia';
 
-export const useUserProductStore = defineStore('user-product', () => {
+export const useProductStore = defineStore('product', {
+  // Using the options API
   state: () => ({
-    list: [{
-      name: 'test',
-      imageUrl: 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp',
-      quantity: 10,
-      about: 'testt',
-      status: 'open',
-      price: 100,
-
-    }],
-    loaded: false
-  });
+    list: [
+      {
+        name: 'test',
+        imageUrl: 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp',
+        quantity: 10,
+        about: 'testt',
+        status: 'open',
+        price: 100,
+      },
+      {
+        name: 'test123',
+        imageUrl: 'https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp',
+        quantity: 10,
+        about: 'testt 123',
+        status: 'open',
+        price: 100,
+      },
+    ],
+    loaded: false,
+  })
 });
--- src/views/User/HomeView.vue
+++ src/views/User/HomeView.vue
@@ -1,29 +1,30 @@
 <script setup>
 import UserLayout from '@/layouts/UserLayout.vue';
+import { useProductStore } from '@/stores/user/product';
+
+const productStore = useProductStore();
 </script>
 
 <template>
-
   <UserLayout>
     <div class="hero bg-base-200 h-96">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">Hello there</h1>
-          <p class="py-6">
-            Hello Easy Shop
-          </p>
+          <p class="py-6">Hello Easy Shop</p>
         </div>
       </div>
     </div>
+    
     <!-- product -->
     <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2">
-      <div v-for="item in [1,2,3,4,5,6,7,8]" class="card bg-base-100 w-full shadow-xl">
+      <div v-for="product in productStore.list" :key="product.name" class="card bg-base-100 w-full shadow-xl">
         <figure>
-          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+          <img :src="product.imageUrl" alt="Shoes" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <h2 class="card-title">{{ product.name }}</h2>
+          <p>{{ product.about }}</p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">Buy Now</button>
           </div>
@@ -32,3 +33,4 @@ import UserLayout from '@/layouts/UserLayout.vue';
     </section>
   </UserLayout>
 </template>
+
