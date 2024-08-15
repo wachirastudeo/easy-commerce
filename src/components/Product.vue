79cf8033cde684a64c7e0f08fa74523d9a8d0cb8
wachira <wachirastudeo@gmail.com>
2024-08-15 10:56:19 +0700

    create Prodcut componants

---
+++ src/components/Product.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {defineProps} from 'vue'
+defineProps({
+    products:Array 
+})
+</script>
+
+<template>
+
+<section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2">
+      <div v-for="product in products" :key="product.name" class="card bg-base-100 w-full shadow-xl">
+        <figure>
+          <img :src="product.imageUrl" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">{{ product.name }}</h2>
+          <p>{{ product.about }}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
--- src/views/User/HomeView.vue
+++ src/views/User/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import UserLayout from '@/layouts/UserLayout.vue';
 import { useProductStore } from '@/stores/user/product';
+import  Product  from "@/components/Product.vue";
 
 const productStore = useProductStore();
 </script>
@@ -17,20 +18,9 @@ const productStore = useProductStore();
     </div>
     
     <!-- product -->
-    <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 m-2">
-      <div v-for="product in productStore.list" :key="product.name" class="card bg-base-100 w-full shadow-xl">
-        <figure>
-          <img :src="product.imageUrl" alt="Shoes" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">{{ product.name }}</h2>
-          <p>{{ product.about }}</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
-    </section>
+     <Product :products="productStore.list">
+     </Product>
+   
   </UserLayout>
 </template>
 
