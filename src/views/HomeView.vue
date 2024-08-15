2de62e5dab50d1be6cf043eb087ce7500e043863
wachira <wachirastudeo@gmail.com>
2024-08-15 08:09:51 +0700

    add theme cupcake

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 
 
 <template>
-  hello world
+<button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>  
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,9 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+
 }
 
