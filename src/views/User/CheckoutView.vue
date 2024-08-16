d0bfd19e0c93ed3d882733813f126b9c372ea996
wachira <wachirastudeo@gmail.com>
2024-08-16 19:18:01 +0700

    add ui check out

--- src/views/User/CheckoutView.vue
+++ src/views/User/CheckoutView.vue
@@ -1,6 +1,9 @@
 <script setup>
 import UserLayout from '@/layouts/UserLayout.vue';
 import { reactive } from 'vue';
+import { useCartStore } from "@/stores/user/cart";
+import { RouterLink } from "vue-router";
+
 const formData = [{
   name: 'Name',
   field: 'name'
@@ -17,6 +20,9 @@ const formData = [{
 },
 ];
 
+const cartStore = useCartStore();
+
+
 const userFormData = reactive({
   name: '',
   email: '',
@@ -24,50 +30,48 @@ const userFormData = reactive({
   note: ''
 
 });
-const payment = ()=>{
-  console.log(userFormData)
-}
+const payment = () => {
+  console.log(userFormData);
+};
 </script>
 
 <template>
 
   <UserLayout>
     Check out Page
-    <div class="flex">
+    <div class="flex py-3">
       <section class="flex-auto w-64 bg-base-200">
-        <div class="py-2 px-8" > 
-          <div
-              v-for="form in formData"
-              class="form-control w-full"
-            >
-              <label class="label">
-                <span class="label-text">{{ form.name }}</span>
-              </label>
-
-              <textarea
-                v-if="form.field === 'address'"
-                class="textarea textarea-bordered h-24"
-                placeholder="Address"
-                v-model="userFormData[form.field]"
-              ></textarea>
-              <input
-                v-else
-                type="text"
-                placeholder="Type here"
-                class="input input-bordered input-sm w-full"
-                v-model="userFormData[form.field]"
-              />
-            </div>
-
-            <button  @click="payment" class="btn btn-neutral my-4 w-full  ">ชำระเงิน </button>
-
-          
+        <div class="py-2 px-8">
+          <div v-for="form in formData" class="form-control w-full">
+            <label class="label">
+              <span class="label-text">{{ form.name }}</span>
+            </label>
+            <textarea v-if="form.field === 'address'" class="textarea textarea-bordered h-24" placeholder="Address"
+              v-model="userFormData[form.field]"></textarea>
+            <input v-else type="text" placeholder="Type here" class="input input-bordered input-sm w-full"
+              v-model="userFormData[form.field]" />
+          </div>
+          <button @click="payment" class="btn btn-neutral my-6 w-full  ">ชำระเงิน </button>
         </div>
-
-
-
+        
       </section>
-      <section class="flex flex-auto w-32 bg-slate-300">Summary</section>
+      <section class="flex-auto w-32 bg-slate-300 px-2 py-4">
+        <div v-for="item in cartStore.items" class="flex py-4 bg-white m-8">
+          <div class="flex-1 px-5">
+            <img :src="item.imageUrl"  alt="">
+          </div>
+          <div class="flex-1">
+            <div class="flex flex-col justify-between h-full">
+              <div>
+                <div> <b>{{ item.name }}</b></div>
+                <div>จำนวน : {{ item.quantity }}</div>
+              </div>
+
+              <div > <RouterLink class="btn btn-neutral" :to="{name:'cart'}"> Edit </RouterLink></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </UserLayout>
 </template>
