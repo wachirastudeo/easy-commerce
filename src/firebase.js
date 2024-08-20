0c529dbaeb4f270e40554b5d008f21e64201ab18
wachira <wachirastudeo@gmail.com>
2024-08-20 09:32:34 +0700

    set firebase sdk

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAnvjUAlP-QW6M236C_9PdqHnfvdDffpCg',
+    authDomain: 'ecommerce-easy-vue.firebaseapp.com',
+    databaseURL: 'https://ecommerce-easy-vue-default-rtdb.asia-southeast1.firebasedatabase.app',
+    projectId: 'ecommerce-easy-vue',
+    storageBucket: 'ecommerce-easy-vue.appspot.com',
+    messagingSenderId: '404262635279',
+    appId: '1:404262635279:web:f2eecd8d497b57e503a07a'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
